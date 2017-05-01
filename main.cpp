8d7e22b5c276cccec6008b4ccaa1d4ef45df0ca6
Zachary Clute <zachclute@gmail.com>
2017-05-01 17:10:05 -0400

    moved creating graph from file to seperate function

--- main.cpp
+++ main.cpp
@@ -12,38 +12,45 @@
 typedef boost::adjacency_matrix<boost::undirectedS> Graph;
 typedef boost::erdos_renyi_iterator<boost::minstd_rand, Graph> ERGen;
 
+bool DEBUG = true;
+
 std::vector<int> find_critical_nodes_given_k(Graph graph, int k) {
   std::vector<int> nodes;
 
   boost::clear_vertex(3, graph);
   boost::clear_vertex(4, graph);
 
-
   return nodes;
 }
 
-int main(void) {
-	int num_nodes, num_edges, size_of_s, u, v; 
+Graph* create_graph_from_input_file(std::string filename) {
+  int num_nodes, num_edges, size_of_s, u, v;
 	std::ifstream input_file;
-	bool debug = true;
+	input_file.open(filename);
+	input_file >> num_nodes >> num_edges >> size_of_s;
+	if(DEBUG) std::cout << "first line: " << num_nodes << " " << num_edges << " " << size_of_s << std::endl;
+  Graph* input_graph = new Graph(num_nodes+1);
+
+	while(input_file >> u >> v) {
+		if(DEBUG) std::cout << "inserting: " << u << " " << v << std::endl;
+    boost::add_edge(u, v, *input_graph);
+	}
+
+	input_file.close();
+  return input_graph;
+}
+
+int main(void) {
+	bool DEBUG = true;
   boost::minstd_rand gen;
 
   //use input file to create graph
-	input_file.open("cnp.in");
 	std::cout << "CMSC 401 project by Zachary Clute" << std::endl;
-	input_file >> num_nodes >> num_edges >> size_of_s;
-	if(debug) std::cout << "first line: " << num_nodes << " " << num_edges << " " << size_of_s << std::endl;
-  Graph* input_graph = new Graph(num_nodes+1);
-	while(input_file >> u >> v) {
-		if(debug) std::cout << "inserting: " << u << " " << v << std::endl;
-    boost::add_edge(u, v, *input_graph);
-	}
-	input_file.close();
-  assert(boost::num_edges(*input_graph) == num_edges);
+  Graph* input_graph = create_graph_from_input_file("cnp.in");
 
   //find the critical nodes for the graph
   std::vector<int> junk = find_critical_nodes_given_k(*input_graph, 4);
-  if(debug) boost::print_graph(*input_graph);
+  if(DEBUG) boost::print_graph(*input_graph);
   delete input_graph;
 
   //generate random graphs
